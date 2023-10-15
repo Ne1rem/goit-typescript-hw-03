7e6a5d2ea3dac74bb83e9c177015c7969b964e5d
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-10-15 16:08:02 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,82 +1,61 @@
-type IsDoorOpen = boolean;
+type IsDoorOpen = "open" | "close"
 
 interface IHouse {
-  door: IsDoorOpen;
+    door: "open" | "close"
 }
 
-abstract class House implements IHouse {
-  public door: IsDoorOpen = false;
-  protected tenants: Person[] = [];
+abstract class House implements IHouse{
+    public door : IsDoorOpen
 
-  comeIn(person: Person): void {
-    if (this.door) {
-      this.tenants.push(person);
-      console.log("Людина ввійшла в будинок.");
-    } else {
-      console.log("Двері закриті. Людина не може ввійти.");
+    protected tenants: Person[] = []
+
+    comeIn(person: Person):void {
+        if(this.door) {
+            this.tenants.push(person)
+            console.log("Людина ввійшла в будинок.");
+        } else {
+            console.log("Двері закриті. Людина не може ввійти.");
+        }
     }
-  }
-
-  public abstract openDoor(key: Key): void;
-
-  public isDoorOpen(): boolean {
-    return this.door;
-  }
-
-  public closeDoor(): void {
-    this.door = false;
-  }
+    public abstract openDoor(key: Key): void 
 }
-
 class Key {
-  private signature: number;
-
-  constructor() {
-    this.signature = Math.random();
-  }
-
-  getSignature() {
-    return this.signature;
-  }
+    private signature: number
+    constructor() {
+        this.signature = Math.random()
+    }
+    getSignature() {
+        return this.signature
+    }
 }
 
 class Person {
-  private key: Key;
-
-  constructor(key: Key) {
-    this.key = key;
-  }
-
-  getKey() {
-    return this.key;
-  }
+    private key : Key
+    constructor(key: Key) {
+        this.key = key;
+      }
+    
+      getKey() {
+        return this.key;
+      }
+    
 }
-
 class MyHouse extends House {
-  public key;
-
-  constructor(key: Key) {
-    super();
-    this.key = key;
-  }
-
-  openDoor(key: Key): void {
-    if (this.isDoorOpen()) {
-      console.log("Двері вже відчинені.");
-    } else if (key.getSignature() === this.key.getSignature()) {
-      this.door = true;
-      console.log("Двері відчинені.");
-    } else {
-      console.log("Ключ не підходить. Двері залишаються закритими.");
+    public key 
+    constructor(key:Key) {
+        super()
+        this.key = key
     }
-  }
+    openDoor(key: Key): void {
+        if(key.getSignature() === this.key.getSignature()){
+            console.log("Двері відчинені.");
+            this.door = 'open'
+        }
+        else {
+            console.log("Ключ не підходить. Двері залишаються закритими.");
+          }
+    }
 }
-
 const key = new Key();
+
 const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-house.comeIn(person);
-
-console.log("Двері відчинені: " + house.isDoorOpen());
